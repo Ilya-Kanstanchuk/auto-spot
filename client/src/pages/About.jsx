e25ec96125f9f8e5f15b42f67c603459b96c4cf7
Ilya-Kanstanchuk <ilyakonst95@gmail.com>
2025-05-07 21:54:45 +0100

    registration form

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,6 +5,26 @@ function About() {
   return (
     <div>
       <Header buttonStatus={"About"} />
+      <div className="mt-10">
+        <h1 className="text-6xl font-bold text-center">About Us</h1>
+        <p className=" text-2xl font-medium px-50 py-8 leading-normal">
+          At AutoSpot, you're just one click away from your dream ride! Whether
+          you're buying your first car or upgrading to something new, we make
+          the entire process smooth, secure, and stress-free. Our mission is to
+          simplify the way people buy and sell cars, offering a trusted platform
+          where every listing is verified and every user can move forward with
+          confidence. We bring buyers and sellers together through an intuitive
+          interface, transparent vehicle information, and helpful tools that
+          make decision-making easier. With AutoSpot, you don’t need to be an
+          expert to find the right vehicle — we’ve built a platform that does
+          the heavy lifting for you. From everyday commuters to luxury vehicles,
+          our growing catalog has something for everyone. We also offer helpful
+          resources, such as buying guides and tips for selling your car, to
+          support you at every step of the journey. Looking for your next car?
+          Explore our listings today and see why AutoSpot is the smart, simple,
+          and secure choice for car lovers everywhere.
+        </p>
+      </div>
     </div>
   );
 }
--- client/src/pages/Registration.jsx
+++ client/src/pages/Registration.jsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 
 function Registration() {
-  return <div>Registration</div>;
+  const API_URL = import.meta.env.VITE_API_URL;
+  const [errorMessage, setErrorMessage] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const responce = await axios.post(`${API_URL}/auth/registration`, {
+        name,
+        email,
+        password,
+      });
+      console.log(responce.data);
+      if (responce.data.success) {
+        navigate("/login");
+      }
+    } catch (error) {
+      setErrorMessage(error.response.data.message);
+      console.log(error);
+    }
+  }
+  return (
+    <div className="min-h-screen bg-loginbg flex justify-center items-center">
+      <div className="bg-white rounded py-8 px-8">
+        <h2 className="block text-center text-3xl font-semibold mb-8">
+          Sign up
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-2">
+            <label className="ml-2" htmlFor="username">
+              Name
+            </label>
+            <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full px-4 py-3 bg-lightgray rounded"
+              type="text"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label className="ml-2" htmlFor="email">
+              Email
+            </label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full px-4 py-3 bg-lightgray rounded"
+              type="email"
+              required
+            />
+          </div>
+          <div className="mb-2">
+            <label className="ml-2" htmlFor="password">
+              Password
+            </label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full px-4 py-3 bg-lightgray rounded"
+              type="password"
+              required
+            />
+          </div>
+          <div className="flex justify-center items-center">
+            {errorMessage && (
+              <div className="text-red-600 text-[17px] text-center mb-4">
+                {errorMessage}
+              </div>
+            )}
+            <button
+              type="submit"
+              className="mt-6 mb-6 py-3 px-20 text-white font-bold text-[15px] bg-mainblue rounded cursor-pointer"
+            >
+              Continue
+            </button>
+          </div>
+          <div className="flex flex-col justify-center items-center">
+            <p>Already have an account?</p>
+            <Link to="/login" className="text-blue-600 font-bold">
+              Sign up now
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Registration;
